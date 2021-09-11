cc706e24cfaf60e1330bc85b8ac7af67c63e223a
Drew Stinnett <drew@drewlink.com>
2021-09-11 09:19:15 -0400

    Updating to v2

--- taskpoet.rb
+++ taskpoet.rb
@@ -1,11 +1,20 @@
 class Taskpoet < Formula
   desc "Alternative to the awesome TaskWarrior, but in Go, with some additional stuff I find useful"
   homepage "https://github.com/drewstinnett/taskpoet"
-  url "https://github.com/drewstinnett/taskpoet/releases/download/v0.0.1/taskpoet-0.0.1_macOS_amd64.tar.gz"
-  sha256 "b38f9716e7594eeef5008a279251c180744e81381c04b3dcda20d7b493147298"
+  url "https://github.com/drewstinnett/taskpoet/releases/download/v0.0.2/taskpoet-0.0.2_macOS_amd64.tar.gz"
+  sha256 "1b0cff7e5b01a85fcf1c9763e87a2584fae4e8be6b0a14e0fafe21eea21bb267"
   license "BSD-2-Clause"
 
   def install
     bin.install 'taskpoet'
   end
+
+  def caveats
+    <<~EOS
+      Add the following in your ~/.zshrc or ~/.profile for faster typin':
+  
+        alias tp="#{opt_bin}/taskpoet"
+    EOS
+  end
+
 end
